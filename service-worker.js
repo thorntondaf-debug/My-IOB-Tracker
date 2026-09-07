f62f0ed18744cb6fe1db7797ee6b4414d08c3c1b
thorntondaf-debug <thorntondaf@gmail.com>
2026-09-08 07:24:34 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,50 @@
-self.addEventListener('fetch',()=>{});
+const CACHE_NAME = "iob-tracker-v1";
+const ASSETS = [
+    "./",
+    "index.html",
+    "style.css",
+    "app.js",
+    "storage.js",
+    "insulin.js",
+    "dashboard.js",
+    "calendar.js",
+    "statistics.js",
+    "manifest.json",
+    "icon-192.png",
+    "icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) =>
+            Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            )
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cached) => {
+            return (
+                cached ||
+                fetch(event.request).then((response) => {
+                    if (response.ok) {
+                        const clone = response.clone();
+                        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+                    }
+                    return response;
+                }).catch(() => cached)
+            );
+        })
+    );
+});
